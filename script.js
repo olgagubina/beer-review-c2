27c37bbd3dec9cf103a528b53fe99a3020e979ab
olgagubina <oagubina@gmail.com>
2017-12-28 21:26:24 +0300

    made modular

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-var beers = [];
+
 var sortclick = 1;
+var app = BeerReviewApp();
 
 // TO ADD BEER TO THE BEERS.array
-function addBeer (name, category, raiting) {
-    beers.push(
-        {
-        "name": name,
-        "category": category,
-        "rate": raiting
-        }
-    );
-}
+// function addBeer (name, category, raiting) {
+//     beers.push(
+//         {
+//         "name": name,
+//         "category": category,
+//         "rate": raiting
+//         }
+//     );
+// }
 
 // TO ADD BEER TO THE HTML-list
-function renderBeers() {
-    $('.beers-list').html('');
-    for (var i=0; i<beers.length; i++) {
-        $('.beers-list').append('<li>' + beers[i].name + ' ' + beers[i].category + '<br>Rate: ' + beers[i].rate + '</li>');
-    }
-}
+// function renderBeers() {
+//     $('.beers-list').html('');
+//     for (var i=0; i<beers.length; i++) {
+//         $('.beers-list').append('<li>' + beers[i].name + ' ' + beers[i].category + '<br>Rate: ' + beers[i].rate + '</li>');
+//     }
+// }
 
 // TO GET POST NEW BEER ITEM
-function postBeer() {
-    addBeer($('.beer-input').val(), $('.category-input').val(), $('#sel1').val());
-    renderBeers();
+// function postBeer() {
+//     addBeer($('.beer-input').val(), $('.category-input').val(), $('#sel1').val());
+//     renderBeers();
 
-    $('.beer-input').val('')
-    $('.category-input').val('');
-    $('#sel1').val('');
-}
+//     $('.beer-input').val('')
+//     $('.category-input').val('');
+//     $('#sel1').val('');
+// }
 
 // TO SORT BEER LIST
 function sortBeer() {
@@ -39,8 +40,54 @@ function sortBeer() {
     renderBeers();
 }
 
-$('.sort-beer').click(sortBeer);
-$('.post-beer').click(postBeer);
+function BeerReviewApp() {
+    var beers = [];
+    function addBeer (name, category, raiting) {
+        beers.push(
+            {
+            "name": name,
+            "category": category,
+            "rate": raiting
+            }
+        );
+    }
+
+    function renderBeers() {
+        $('.beers-list').html('');
+        for (var i=0; i<beers.length; i++) {
+            $('.beers-list').append('<li>' + beers[i].name + ' ' + beers[i].category + '<br>Rate: ' + beers[i].rate + '</li>');
+        }
+    }
+
+    function sortBeer() {
+        beers.sort(function(a,b) {
+            return ((a.rate-b.rate)*sortclick);
+        });
+        sortclick= -sortclick;
+        renderBeers();
+    }
+
+    return {
+        addBeer: addBeer,
+        updateBeers: renderBeers,
+        sortBeer: sortBeer
+    }
+}
+
+$('.sort-beer').click(function sort() {
+                        app.sortBeer();
+});
+
+$('.post-beer').click(function post(){
+                        var name = $('.beer-input').val();
+                        var category = $('.category-input').val();
+                        var raiting = $('#sel1').val();
+                        app.addBeer(name, category, raiting);
+                        app.updateBeers();
+                        $('.beer-input').val('')
+                        $('.category-input').val('');
+                        $('#sel1').val('');
+});
 
  
 
